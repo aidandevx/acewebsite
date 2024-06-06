45c2bd6bdc1b4a9302e2b8cb16f393ad9c76da52
Aidan <147556918+aidandevx@users.noreply.github.com>
2024-06-06 18:21:22 +0100

    Update JoinNow.js

--- src/components/JoinNow.js
+++ src/components/JoinNow.js
@@ -11,7 +11,7 @@ const JoinNow = () => {
           Invite our bot to your Discord server!{" "}
         </div>
         <button className="btn">
-          <Link className="h-color no-decoration" href="#">
+          <Link className="h-color no-decoration" href="https://discord.com/oauth2/authorize?client_id=1248325734188056687">
             Invite now!
           </Link>
         </button>
