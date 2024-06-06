79f4fe24ba6ed9e150f72b3cd46d59934185c0fd
Aidan <147556918+aidandevx@users.noreply.github.com>
2024-06-06 18:51:13 +0100

    Update About.js

--- src/components/About.js
+++ src/components/About.js
@@ -5,7 +5,7 @@ const About = () => {
     <>
       <section className="about-section" id="about-section">
         <div className="about-header">
-          <h3 className="h-color">Why use this bot?</h3>
+          <h3 className="h-color">Why use these bots?</h3>
         </div>
         <div className="about-container">
           <div className="about-card">
