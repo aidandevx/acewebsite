b7fbf725881a1036056b7224fd352897f11c98d3
Aidan <147556918+aidandevx@users.noreply.github.com>
2024-06-06 18:22:42 +0100

    Update JoinNow.js

--- src/components/JoinNow.js
+++ src/components/JoinNow.js
@@ -5,7 +5,7 @@ const JoinNow = () => {
     <>
       <section className="joinUs">
         <div className="h-color joinUs-header">
-          <h3>Join us now!</h3>
+          <h3>Join us no!</h3>
         </div>
         <div className="joinUs-para p-color">
           Invite our bot to your Discord server!{" "}
